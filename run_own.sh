30ba95591080a6874f0960d00453a2a94a10605e
Christoph Blattgerste <blattgerste@stud.uni-heidelberg.de>
2023-10-06 15:24:36 +0200

    added dicom converter to run script and adjusted input as argparse

--- run_own.sh
+++ run_own.sh
@@ -6,6 +6,8 @@ GPU_NUMBER=0
 MODEL_INDEX='2'
 
 MODEL_PATH='models/'
+DICOM_FOLDER='../Data/BSC-DBT'
+DICOM_FILE='1-1.dcm'
 DATA_FOLDER='../Data/BSC-DBT/images'
 INITIAL_EXAM_LIST_PATH='../Data/BSC-DBT/exam_list.pkl'
 CROPPED_IMAGE_PATH='../Data/BSC-DBT/output/cropped_images'
@@ -13,25 +15,32 @@ CROPPED_EXAM_LIST_PATH='../Data/BSC-DBT/output/cropped_images/cropped_exam_list.
 SEG_PATH='../Data/BSC-DBT/segmentation'
 EXAM_LIST_PATH='../Data/BSC-DBT/output/data.pkl'
 OUTPUT_PATH='../Data/BSC-DBT/output'
+
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
+echo 'Stage 1: Convert DICOM exams'
+python3 src/dicom/convert_dicom.py \
+    --dicom-data-folder $DICOM_FOLDER \
+    --dicom-file $DICOM_FILE \
+    --image-data-folder $DATA_FOLDER \
+    --exam-list-path $INITIAL_EXAM_LIST_PATH
 
-echo 'Stage 1: Crop Mammograms'
+echo 'Stage 2: Crop Mammograms'
 python3 src/cropping/crop_mammogram.py \
-    --input-data-folder $DATA_FOLDER \
+    --image-data-folder $DATA_FOLDER \
     --output-data-folder $CROPPED_IMAGE_PATH \
     --exam-list-path $INITIAL_EXAM_LIST_PATH  \
-    --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH  \
+    --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
     --num-processes $NUM_PROCESSES
 
-echo 'Stage 2: Extract Centers'
+echo 'Stage 3: Extract Centers'
 python3 src/optimal_centers/get_optimal_centers.py \
     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
     --data-prefix $CROPPED_IMAGE_PATH \
     --output-exam-list-path $EXAM_LIST_PATH \
     --num-processes $NUM_PROCESSES
 
-echo 'Stage 3: Run Classifier'
+echo 'Stage 4: Run Classifier'
 python3 -m cProfile -o model.prof src/scripts/run_model.py \
     --model-path $MODEL_PATH \
     --data-path $EXAM_LIST_PATH \
@@ -42,4 +51,3 @@ python3 -m cProfile -o model.prof src/scripts/run_model.py \
     --gpu-number $GPU_NUMBER \
     --model-index $MODEL_INDEX \
     # --visualization-flag
-
