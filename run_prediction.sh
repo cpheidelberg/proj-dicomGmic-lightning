5bd63d948163a4074634289b3af079df02bd2117
Christoph Blattgerste <christoph.blattgerste@med.uni-heidelberg.de>
2024-01-18 17:19:55 +0000

    prediction with self-trained model

--- run_prediction.sh
+++ run_prediction.sh
@@ -5,46 +5,47 @@ DEVICE_TYPE='cpu'
 GPU_NUMBER=0
 MODEL_INDEX='2'
 
-MODEL_PATH='models/'
-DICOM_FOLDER='sample_data/dicom_exams'
+MODEL_PATH="tb_logs/GMIC_cat/version_12/checkpoints/epoch=63-step=64000.ckpt"
+DICOM_FOLDER='test_data_vindr/dicom_exams'
 DICOM_FILE='1-1.dcm'
 # DICOM_FILE='Example-Image.dcm'
-DATA_FOLDER='sample_data/images'
-INITIAL_EXAM_LIST_PATH='sample_data/exam_list.pkl'
-CROPPED_IMAGE_PATH='sample_output/cropped_images'
-CROPPED_EXAM_LIST_PATH='sample_output/cropped_images/cropped_exam_list.pkl'
-SEG_PATH='sample_output/segmentation'
-EXAM_LIST_PATH='sample_output/data.pkl'
-OUTPUT_PATH='sample_output'
+DATA_FOLDER='test_data_vindr/images'
+INITIAL_EXAM_LIST_PATH='test_data_vindr/exam_list.pkl'
+CROPPED_IMAGE_PATH='test_data_vindr/cropped_images'
+CROPPED_EXAM_LIST_PATH='test_data_vindr/cropped_images/cropped_exam_list.pkl'
+SEG_PATH='test_data_vindr/segmentation'
+EXAM_LIST_PATH='test_data_vindr/data.pkl'
+OUTPUT_PATH='test_data_vindr'
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
 start_time=$(date +%s)
 
-# echo 'Stage 1: Convert DICOM exams'
-# python3 src/dicom/convert_dicom.py \
-#     --dicom-data-folder $DICOM_FOLDER \
-#     --dicom-file $DICOM_FILE \
-#     --image-data-folder $DATA_FOLDER \
-#     --exam-list-path $INITIAL_EXAM_LIST_PATH
-#
-# echo 'Stage 2: Crop Mammograms'
-# python3 src/cropping/crop_mammogram.py \
-#     --input-data-folder $DATA_FOLDER \
-#     --output-data-folder $CROPPED_IMAGE_PATH \
-#     --exam-list-path $INITIAL_EXAM_LIST_PATH  \
-#     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
-#     --num-processes $NUM_PROCESSES
-#
-# echo 'Stage 3: Extract Centers'
-# python3 src/optimal_centers/get_optimal_centers.py \
-#     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
-#     --data-prefix $CROPPED_IMAGE_PATH \
-#     --output-exam-list-path $EXAM_LIST_PATH \
-#     --num-processes $NUM_PROCESSES
+echo 'Stage 1: Convert DICOM exams'
+# use convert_dicom.py for DBT dataset, convert_dicom2.py script for vindr dataset
+python3 src/dicom/convert_dicom2.py \
+    --dicom-data-folder $DICOM_FOLDER \
+    --dicom-file $DICOM_FILE \
+    --image-data-folder $DATA_FOLDER \
+    --exam-list-path $INITIAL_EXAM_LIST_PATH
+
+echo 'Stage 2: Crop Mammograms'
+python3 src/cropping/crop_mammogram.py \
+    --input-data-folder $DATA_FOLDER \
+    --output-data-folder $CROPPED_IMAGE_PATH \
+    --exam-list-path $INITIAL_EXAM_LIST_PATH  \
+    --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
+    --num-processes $NUM_PROCESSES
+
+echo 'Stage 3: Extract Centers'
+python3 src/optimal_centers/get_optimal_centers.py \
+    --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
+    --data-prefix $CROPPED_IMAGE_PATH \
+    --output-exam-list-path $EXAM_LIST_PATH \
+    --num-processes $NUM_PROCESSES
 
  echo 'Stage 4: Run Classifier'
- python3 src/scripts/run_model.py \
+ python3 src/scripts/test_model.py \
      --model-path $MODEL_PATH \
      --dicom-file $DICOM_FILE \
      --data-path $EXAM_LIST_PATH \
@@ -53,15 +54,14 @@ start_time=$(date +%s)
      --output-path $OUTPUT_PATH \
      --device-type $DEVICE_TYPE \
      --gpu-number $GPU_NUMBER \
-     --model-index $MODEL_INDEX \
      --visualization-flag
 
-echo 'Stage 5: Create DICOM SR exams'
-python3 src/dicom/create_dicomsr.py \
-    --segmentation-path $SEG_PATH \
-    --result-path $OUTPUT_PATH \
-    --dicom-file $DICOM_FILE \
-    --exam-list-path $INITIAL_EXAM_LIST_PATH \
+# echo 'Stage 5: Create DICOM SR exams'
+# python3 src/dicom/create_dicomsr.py \
+#     --segmentation-path $SEG_PATH \
+#     --result-path $OUTPUT_PATH \
+#     --dicom-file $DICOM_FILE \
+#     --exam-list-path $INITIAL_EXAM_LIST_PATH \
 
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
