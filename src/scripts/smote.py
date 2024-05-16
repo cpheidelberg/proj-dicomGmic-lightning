6dede96b08a509a711e7c99f3783c922f7535326
Richard Blažek <blazek.risa@gmail.com>
2024-05-16 17:23:36 +0200

    SMOTE in one class

--- src/scripts/smote.py
+++ src/scripts/smote.py
@@ -2,36 +2,32 @@ import numpy as np
 import scipy.spatial
 
 
-class NearestNeighbours:
+class SMOTE:
     def __init__(self, points: np.ndarray):
         self._points = points
         self._tree = scipy.spatial.KDTree(points)
-    
-    def find(self, point, k) -> np.ndarray:
+        self._rng = np.random.default_rng()
+
+    def _nearest_neighbours(self, point: np.ndarray, k: int) -> np.ndarray:
         _, indices = self._tree.query(point, k=k+1)
         return self._points[indices[1:]]
 
+    def _sample_with_replacement(self, items: np.ndarray, size: int):
+        return items[self._rng.integers(0, len(items), size)]
 
-_rng = np.random.default_rng()
+    def _random_linear_combination(self, u: np.ndarray, v: np.ndarray):
+        return u + (v - u) * self._rng.random(len(v))
 
-def _sample_with_replacement(items: np.ndarray, size: int):
-    return items[_rng.integers(0, len(items), size)]
+    def generate(self, n: int, k: int):
+        point_num, attr_num = self._points.shape
+        result = np.zeros([point_num * n, attr_num])
 
-def _random_linear_combination(u: np.ndarray, v: np.ndarray):
-    return u + (v - u) * _rng.random(len(v))
+        i = 0
+        for point in self._points:
+            nearest = self._nearest_neighbours(point, k)
+            selected = self._sample_with_replacement(nearest, n)
+            for neighbour in selected:
+                result[i] = self._random_linear_combination(point, neighbour)
+                i += 1
 
-
-def smote(points: np.ndarray, mul: int, k: int) -> np.ndarray:
-    point_count, attr_count = points.shape
-
-    synthetic = np.zeros([point_count * mul, attr_count])
-    nn = NearestNeighbours(points)
-
-    i = 0
-    for pt in points:
-        selected = _sample_with_replacement(nn.find(pt, k), mul)
-        for nb in selected:
-            synthetic[i] = _random_linear_combination(pt, nb)
-            i += 1
-
-    return synthetic
+        return result
