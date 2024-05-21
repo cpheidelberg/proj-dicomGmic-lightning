1c5d414c66df6386019de609bf7c6c2ecb9469de
Richard Blažek <blazek.risa@gmail.com>
2024-05-21 13:38:26 +0200

    Feature vector storage

---
+++ src/data/feature_vector_storage.py
@@ -0,0 +1,24 @@
+import numpy as np
+import io
+import base64
+
+class FeatureVectorStorage:
+    def __init__(self, path: str):
+        self._path = path
+
+    def add(self, label: int, vector: np.ndarray):
+        with io.BytesIO() as buf, open(self._path, mode='a') as file:
+            np.save(buf, vector)
+            file.write(f'{label} {base64.b64encode(buf.getvalue()).decode()}\n')
+
+    def add_many(self, labels: np.ndarray, vectors: np.ndarray):
+        for label, vector in zip(labels, vectors):
+            self.add(label, vector)
+
+    def read(self):
+        def parse(line: str) -> tuple[int, np.ndarray]:
+            label, vector = line.split(' ', maxsplit=1)
+            return int(label), np.load(io.BytesIO(base64.b64decode(vector)))
+
+        with open(self._path) as file:
+            return (parse(line) for line in file if line)
