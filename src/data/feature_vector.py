8400e5b5d373aad8bee9a014fc69bc20c7987ce3
Richard Blažek <blazek.risa@gmail.com>
2024-05-24 18:06:41 +0200

    Print type for unmarshall

--- src/data/feature_vector.py
+++ src/data/feature_vector.py
@@ -67,7 +67,7 @@ class Storage(torch.utils.data.Dataset):
     def _marshall(self, global_vec: np.ndarray, h_crops: np.ndarray):
         self._global_vec_shape = global_vec.shape
         self._h_crops_shape = h_crops.shape
-        print(f'{global_vec.dtype=}; {h_crops.dtype=}')
+        print(f'Marshall {global_vec.dtype=}; {h_crops.dtype=}')
         return pickle.dumps(np.concatenate((global_vec.flatten(), h_crops.flatten())))
 
     def _unmarshall(self, vector: bytes):
@@ -75,6 +75,7 @@ class Storage(torch.utils.data.Dataset):
         split_index = np.prod(self._global_vec_shape)
         global_vec = vector[:split_index].reshape(self._global_vec_shape)
         h_crops = vector[split_index:].reshape(self._h_crops_shape)
+        print(f'Unmarshall {global_vec.dtype=}; {h_crops.dtype=}')
         return global_vec, h_crops
 
     def reset(self):
