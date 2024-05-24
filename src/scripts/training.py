5feffa55f30f1915fe04679794d1f8826f5394c9
Richard Blažek <blazek.risa@gmail.com>
2024-05-24 11:03:45 +0200

    Bugfix

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     # data = dataset.H5Dataset(h5_filepath=h5_path, relevant_labels=["No Finding", "Mass", "Suspicious Calcification"])
     dataTrain, dataValid, dataTest = random_split(data, [0.8, 0.1, 0.1])
 
-    feature_vector_storage = feature_vector.Storage(feature_vectors_path, data.no_finding_idx)
+    feature_vector_storage = feature_vector.Storage(feature_vectors_path, data.no_finding_idx, parameters["num_classes"])
 
     # Training
     gmic_module = gmic.GMIC(
