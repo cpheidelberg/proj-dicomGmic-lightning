250246036b3d6cc69b66ee4141418b2096b7693a
Richard Blažek <blazek.risa@gmail.com>
2024-05-29 22:10:31 +0200

    Named arg

--- src/scripts/png_to_binary.py
+++ src/scripts/png_to_binary.py
@@ -31,7 +31,7 @@ def _read_csv_info(image_dir: str, dict_path: str, top_c: int):
     category_dict = {category: sum(image['category'] == category for image in images) for category in category_set}
     images.sort(key=lambda image: category_dict[image['category']])
 
-    category_names = np.array(sorted(category_set, lambda name: -category_dict[name])[:top_c], dtype='object')
+    category_names = np.array(sorted(category_set, key=lambda name: -category_dict[name])[:top_c], dtype='object')
     category_sizes = np.array([category_dict[name] for name in category_names], dtype=np.int32)
     return category_names, category_sizes, images
 
