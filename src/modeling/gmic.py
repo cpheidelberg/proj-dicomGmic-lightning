fece3c6c04ae27caba2ea83de4e87adb732885ef
Richard Blažek <blazek.risa@gmail.com>
2024-05-31 13:26:41 +0200

    Fixed typo

--- src/modeling/gmic.py
+++ src/modeling/gmic.py
@@ -43,7 +43,7 @@ class GMIC(lightning.LightningModule):
         self.train_auc = metrics.BinaryAUROC()
 
         if class_weights:
-            weight_tensor = torch.FloatTensor([class_weights] * parameters['batch_size']).to(parameters['parameters'])
+            weight_tensor = torch.FloatTensor([class_weights] * parameters['batch_size']).to(parameters['device'])
         else:
             weight_tensor = None
 
