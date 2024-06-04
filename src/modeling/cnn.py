1a550518b5c4cf1a215f579f2e184c0077deb3ea
Richard Blažek <blazek.risa@gmail.com>
2024-06-04 10:45:02 +0200

    Code moved out of CNN

--- src/modeling/cnn.py
+++ src/modeling/cnn.py
@@ -18,9 +18,8 @@
 # ==============================================================================
 
 import torch
-import numpy as np
 import src.modeling.modules as m
-import src.utilities.tools as utils
+import src.utilities.tools as croptools
 
 
 class CNN(torch.nn.Module):
@@ -42,51 +41,6 @@ class CNN(torch.nn.Module):
         self.dn_resnet = self.local_network.dn_resnet
 
 
-    def _convert_crop_position(self, crops_x_small, cam_size, x_original):
-        """
-        Function that converts the crop locations from cam_size to x_original
-        :param crops_x_small: N, k*c, 2 numpy matrix
-        :param cam_size: (h,w)
-        :param x_original: N, C, H, W pytorch variable
-        :return: N, k*c, 2 numpy matrix
-        """
-        # retrieve the dimension of both the original image and the small version
-        h, w = cam_size
-        _, _, H, W = x_original.size()
-
-        # interpolate the 2d index in h_small to index in x_original
-        top_k_prop_x = crops_x_small[:, :, 0] / h
-        top_k_prop_y = crops_x_small[:, :, 1] / w
-        # sanity check
-        assert np.max(top_k_prop_x) <= 1.0, "top_k_prop_x >= 1.0"
-        assert np.min(top_k_prop_x) >= 0.0, "top_k_prop_x <= 0.0"
-        assert np.max(top_k_prop_y) <= 1.0, "top_k_prop_y >= 1.0"
-        assert np.min(top_k_prop_y) >= 0.0, "top_k_prop_y <= 0.0"
-        # interpolate the crop position from cam_size to x_original
-        top_k_interpolate_x = np.expand_dims(np.around(top_k_prop_x * H), -1)
-        top_k_interpolate_y = np.expand_dims(np.around(top_k_prop_y * W), -1)
-        top_k_interpolate_2d = np.concatenate([top_k_interpolate_x, top_k_interpolate_y], axis=-1)
-        return top_k_interpolate_2d
-
-
-    def _retrieve_crop(self, x_original, crop_positions, crop_method):
-        """
-        Function that takes in the original image and cropping position and returns the crops
-        :param x_original_pytorch: PyTorch Tensor array (N,C,H,W)
-        :param crop_positions:
-        :return:
-        """
-        batch_size, num_crops, _ = crop_positions.shape
-        crop_h, crop_w = self._crop_shape
-
-        output = torch.ones((batch_size, num_crops, crop_h, crop_w)).type_as(x_original)
-
-        for i in range(batch_size):
-            for j in range(num_crops):
-                utils.crop_pytorch(x_original[i, 0, :, :], self._crop_shape, crop_positions[i,j,:], output[i,j,:,:], method=crop_method)
-        return output
-
-
     def forward(self, x_original):
         """
         :param x_original: N x H x W x C array
@@ -102,10 +56,10 @@ class CNN(torch.nn.Module):
         small_x_locations = self.retrieve_roi_module.forward(x_original, self._cam_size, self.saliency_map)
 
         # convert crop locations that is on self.cam_size to x_original
-        self.patch_locations = self._convert_crop_position(small_x_locations, self._cam_size, x_original)
+        self.patch_locations = croptools.scale_crops(small_x_locations, self._cam_size, x_original.size()[-2:])
 
         # patch retriever
-        crops_variable = self._retrieve_crop(x_original, self.patch_locations, self.retrieve_roi_module.crop_method)
+        crops_variable = croptools.retrieve_crops(x_original, self.patch_locations, self._crop_shape, self.retrieve_roi_module.crop_method)
         self.patches = crops_variable.data.cpu().numpy()
 
         # detection network
