cf07d5dae51649c2457a89cba36860e68d1cb273
Richard Blažek <blazek.risa@gmail.com>
2024-06-05 13:06:57 +0200

    Just save feature vectors

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         # training related hyper-parameters
         "device_type": device,
         "gpu_number": 0,
-        "epochs": 8,
+        "epochs": 4,
         "batch_size": 16,
         "learning_rate": 1e-3,
         "pretrained": True,
