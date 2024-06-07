ca3d6ec1e7f5aca3cdc3781bf7352f3ebe91f96c
Richard Blažek <blazek.risa@gmail.com>
2024-06-07 10:56:23 +0200

    Try larger batch

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         "device_type": device,
         "gpu_number": 0,
         "epochs": 8,
-        "batch_size": 16,
+        "batch_size": 32,
         "learning_rate": 1e-3,
         "pretrained": True,
         "fine-tuning": False,
