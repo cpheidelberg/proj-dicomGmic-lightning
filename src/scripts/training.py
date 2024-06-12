584d1bebb9a7af258b2b4a18151841d8f7926d5f
Richard Blažek <blazek.risa@gmail.com>
2024-06-12 17:49:31 +0200

    Run three times

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -99,4 +99,11 @@ def run_training(epochs: int, undersampling_rate: float, augmentation_rate: floa
 
 
 if __name__ == "__main__":
+    print('version_0: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.5, augmentation_rate=0.5, smote_rate=0.9)')
     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.5, augmentation_rate=0.5, smote_rate=0.9)
+
+    print('version_1: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.5, smote_rate=0.9)')
+    run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.5, smote_rate=0.9)
+
+    print('version_2: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.9)')
+    run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.9)
