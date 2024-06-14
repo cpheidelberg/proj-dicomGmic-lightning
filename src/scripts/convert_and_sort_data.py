82f22213314d33b75df85c014f174f4cd0fa5f44
Richard Blažek <blazek.risa@gmail.com>
2024-06-14 14:48:35 +0200

    Refactored the sorting script

--- src/scripts/convert_and_sort_data.py
+++ src/scripts/convert_and_sort_data.py
@@ -18,37 +18,35 @@ def _process_image(src_dir: str, dst_dir: str, line: pd.Series, category_name: s
     image = loading.read_image(os.path.join(src_dir, src_name), dtype=None)
     image = loading.flip_and_crop(image, view, horizontal_flip, best_center)
     loading.write_image(os.path.join(dst_dir, dst_name), image)
-    return dst_name, dcm_name, category_name
+    return [dst_name, dcm_name, category_name]
 
 
 def convert_vindr_mammo_dataset_to_our_storage_format():
-    parent_path = '/home/student/sdsHD/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output'
-    image_dir = os.path.join(parent_path, 'cropped_images')
-    dict_path = os.path.join(parent_path, 'dictionary.csv')
+    src_dir = '/home/student/sdsHD/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output/cropped_images'
+    dictionary_path = '/home/student/sdsHD/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output/dictionary.csv'
     top_c = 6
 
-    src = pd.read_csv(dict_path, converters={'best_center': ast.literal_eval, 'finding_categories': ast.literal_eval})
-    dst = pd.DataFrame({'png': [], 'dicom': [], 'label': []}, dtype=str)
+    dctn = pd.read_csv(dictionary_path, converters={'best_center': ast.literal_eval, 'finding_categories': ast.literal_eval})
+    mapp = pd.DataFrame({'png': [], 'dicom': [], 'label': []}, dtype=str)
 
     # Delete images which don't have exactly one category
-    for i in range(len(src)):
-        if len(src.loc[i, 'finding_categories']) != 1:
-            src.drop(i, inplace=True)
+    for i in range(len(dctn)):
+        if len(dctn.loc[i, 'finding_categories']) != 1:
+            dctn.drop(i, inplace=True)
         else:
-            src.loc[i, 'finding_categories'] = src.loc[i, 'finding_categories'][0]
+            dctn.loc[i, 'finding_categories'] = dctn.loc[i, 'finding_categories'][0]
 
-    src.reset_index(inplace=True)
-    categories = list(src['finding_categories'].value_counts()[:top_c].keys())
+    dctn.reset_index(inplace=True)
+    categories = list(dctn['finding_categories'].value_counts()[:top_c].keys())
 
-    dest_path = '/home/student/gmic/vindrmammo_data'
-    for index, category in enumerate(categories):
-        os.makedirs(os.path.join(dest_path, str(index)), exist_ok=True)
+    dst_dir = '/home/student/gmic/vindrmammo_data'
+    for category_index, category_name in enumerate(categories):
+        os.makedirs(os.path.join(dst_dir, str(category_index)), exist_ok=True)
 
-        for line in src[src['finding_categories'] == category].iloc:
-            png_name, dicom_name, label_name = _process_image(image_dir, dest_path, line, category, index)
-            dst.loc[len(dst), :] = [png_name, dicom_name, label_name]
+        for line in dctn[dctn['finding_categories'] == category_name].iloc:
+            mapp.loc[len(mapp), :] = _process_image(src_dir, dst_dir, line, category_name, category_index)
 
-    dst.to_csv(os.path.join(dest_path, 'mapping.csv'))
+    mapp.to_csv(os.path.join(dst_dir, 'mapping.csv'))
 
 
 if __name__ == "__main__":
