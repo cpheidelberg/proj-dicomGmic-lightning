2f55d88b3636a2757bc0a7d5f4fe6c4afcbdc98e
Richard Blažek <blazek.risa@gmail.com>
2024-06-17 16:09:47 +0200

    Script to convert OMI dataset

--- src/scripts/convert_omi_sample_dicoms.py
+++ src/scripts/convert_omi_sample_dicoms.py
@@ -1,81 +1,110 @@
-import numpy as np
-import pydicom as dcm
+import pydicom
+import pandas as pd
 import os, json, sys
-import imageio.v3 as imageio
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = "/".join(current_dir.split("/")[:-2])
 sys.path.append(parent_dir)
 
 import src.cropping.crop_mammogram as cropping
-import src.optimal_centers.get_optimal_centers as optimal_centers
+import src.optimal_centers.get_optimal_centers as centers
+import src.data.loading as loading
 
 
-def get_view(file: dcm.FileDataset):
-    return f'{file.ImageLaterality}-{file.ViewPosition}'
+class _dicom:
+    def get_category(self, file: pydicom.FileDataset, data: dict):
+        screening = data.get('SCREENING', {}).get(file.ImageLaterality, {})
+        if screening.get('Mass'):
+            return 'Mass'
+        elif screening.get('Microcalcification') or screening.get('MicrocalcWithMass'):
+            return 'Suspicious Calcification'
+        else:
+            return 'No Finding'
 
-
-def get_best_center(image: np.ndarray, view: str, horizontal_flip: str):
-    mode = 'right' if view.startswith('R') else 'left'
-    crop = cropping.crop_img_from_largest_connected(image, mode)
-    datum = {
-        'window_location': crop[0],
-        'rightmost_points': crop[1],
-        'bottommost_points': crop[2],
-        'distance_from_starting_side': crop[3],
-        'full_view': view,
-        'view': view[2:],
-        'horizontal_flip': horizontal_flip
-    }
-    return optimal_centers.extract_center(datum, image)
-
-
-def get_category(file: dcm.FileDataset, data: dict):
-    screening = data.get('SCREENING', {}).get(file.ImageLaterality, {})
-    if screening.get('Mass'):
-        return 'Mass'
-    elif screening.get('Microcalcification') or screening.get('MicrocalcWithMass'):
-        return 'Suspicious Calcification'
-    else:
-        return 'No Finding'
-
-
-def convert_file(path: str, data: dict):
-    with dcm.read_file(path) as file:
-        horizontal_flip = file.FieldOfViewHorizontalFlip
-        view = get_view(file)
-
-        return {
-            'path': path,
-            'horizontal_flip': horizontal_flip,
-            'view': view,
-            'best_center': get_best_center(file.pixel_array, view, horizontal_flip),
-            'category': get_category(file, data)
+    def get_center(self):
+        mode = 'right' if self.view.startswith('R') else 'left'
+        crop = cropping.crop_img_from_largest_connected(self.image, mode)
+        datum = {
+            'window_location': crop[0],
+            'rightmost_points': crop[1],
+            'bottommost_points': crop[2],
+            'distance_from_starting_side': crop[3],
+            'full_view': self.view,
+            'view': self.view[2:],
+            'horizontal_flip': self.horizontal_flip
         }
+        return centers.extract_center(datum, self.image)
+
+    def __init__(self, path: str, data: dict):
+        with pydicom.read_file(path) as file:
+            self.path = path
+            self.image = file.pixel_array
+            self.view = f'{file.ImageLaterality}-{file.ViewPosition}'
+            self.horizontal_flip = 'YES' if file.FieldOfViewHorizontalFlip == 'YES' else 'NO'
+            self.category = self.get_category(file, data)
+            self.center = self.get_center()
 
 
-def convert_patient(root: str, patient: str):
+def _process_dicom(path: str, data: dict) -> list[_dicom]:
+    try:
+        return [_dicom(path, data)]
+    except RuntimeError:
+        return []
+
+
+def _process_patient(root: str, patient: str):
     with open(os.path.join(root, 'DATA', patient, f'NBSS_{patient}.json')) as file:
         episodes = [value for value in json.load(file).values() if isinstance(value, dict)]
 
     studies = [(study, data) for data in episodes for study in data['StudyList'].split(',') if study]
     paths = [(e.path, data) for study, data in studies for e in os.scandir(os.path.join(root, 'IMAGES', patient, study))]
 
-    result = []
-    for path, data in paths:
-        try:
-            result.append(convert_file(path, data))
-        except RuntimeError:
-            pass
-    return result
+    return [dcm for path, data in paths for dcm in _process_dicom(path, data)]
 
 
-def convert_all(root: str) -> list:
-    result = []
-    for patient in os.listdir(os.path.join(root, 'IMAGES')):
-        result += convert_patient(root, patient)
-    return result
+def _process_patients(root: str):
+    return [dcm for patient in os.listdir(os.path.join(root, 'IMAGES')) for dcm in _process_patient(root, patient)]
 
 
-the_root = '/home/student/sdsHD/sd21c015/DataBaseMammography/OMI_SAMPLE'
+def _sorted_categories(dicoms: list[_dicom]):
+    sizes = {}
+    for dcm in dicoms:
+        sizes[dcm.category] = sizes.get(dcm.category, 0) + 1
+    return sorted(sizes.keys(), key=lambda category: -sizes[category])
 
+
+def _divide_dicoms(dicoms: list[_dicom], categories: list[str]) -> list[list[_dicom]]:
+    split = [[] for _ in categories]
+    for dcm in dicoms:
+        split[categories.index(dcm.category)].append(dcm)
+    return split
+
+
+def _save_image(dcm: _dicom, category_index: int, category_name: str, dst_dir: str, index: int):
+    dst_name = f'{category_index}/{index}.png'
+    dcm_name = '/'.join(dcm.path.split('/')[-3:])
+
+    image = loading.flip_and_crop(dcm.image, dcm.view, dcm.horizontal_flip, dcm.center)
+    loading.write_image(os.path.join(dst_dir, dst_name), image)
+
+    return [dst_name, dcm_name, category_name]
+
+
+def main(src_dir: str, dst_dir: str):
+    dicoms = _process_patients(src_dir)
+    categories = _sorted_categories(dicoms)
+    dicoms = _divide_dicoms(dicoms, categories)
+
+    mapp = pd.DataFrame({'png': [], 'dicom': [], 'label': []}, dtype=str)
+
+    for category_index, category_name in enumerate(categories):
+        os.makedirs(os.path.join(dst_dir, str(category_index)), exist_ok=True)
+
+        for i, dcm in enumerate(dicoms[category_index]):
+            mapp.loc[len(mapp), :] = _save_image(dcm, category_index, category_name, dst_dir, i)
+
+    mapp.to_csv(os.path.join(dst_dir, 'mapping.csv'))
+
+
+if __name__ == '__main__':
+    main('/home/student/sdsHD/sd21c015/DataBaseMammography/OMI_SAMPLE', '/home/student/gmic/omisample_data')
