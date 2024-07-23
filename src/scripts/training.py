cd58e4427da198a22859e1b4b097c50807267a85
Richard Blažek <blazek.risa@gmail.com>
2024-07-23 10:53:34 +0100

    Just very basic training

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -98,21 +98,24 @@ def run_training(epochs: int, undersampling_rate: float, augmentation_rate: floa
     training.test(model)
 
 
+# if __name__ == "__main__":
+#     print('version_1: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0)')
+#     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0)
+
+#     print('version_2: run_training(epochs=16, epoch_smote=16, undersampling_rate=1.0, augmentation_rate=0.0, smote_rate=0.0)')
+#     run_training(epochs=16, epoch_smote=16, undersampling_rate=1.0, augmentation_rate=0.0, smote_rate=0.0)
+
+#     print('version_3: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.0)')
+#     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.0)
+
+#     print('version_4: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=1.0)')
+#     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=1.0)
+
+#     print('version_5: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5)')
+#     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5)
+
+#     print('version_6: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.4, smote_rate=0.5)')
+#     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.4, smote_rate=0.5)
+
 if __name__ == "__main__":
-    print('version_1: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0)')
-    run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0)
-
-    print('version_2: run_training(epochs=16, epoch_smote=16, undersampling_rate=1.0, augmentation_rate=0.0, smote_rate=0.0)')
-    run_training(epochs=16, epoch_smote=16, undersampling_rate=1.0, augmentation_rate=0.0, smote_rate=0.0)
-
-    print('version_3: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.0)')
-    run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=1.0, smote_rate=0.0)
-
-    print('version_4: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=1.0)')
-    run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=1.0)
-
-    print('version_5: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5)')
-    run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5)
-
-    print('version_6: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.4, smote_rate=0.5)')
-    run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.4, smote_rate=0.5)
+    run_training(epochs=8, epoch_smote=8, undersampling_rate=1.0, augmentation_rate=0.0, smote_rate=0.0)
