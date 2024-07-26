9723a3c7fa54d6f277907ef1bc9821eed07b5b88
Richard Blažek <blazek.risa@gmail.com>
2024-07-26 10:24:24 +0100

    Fixed paths for different server

--- src/scripts/convert_omi_db_dicoms.py
+++ src/scripts/convert_omi_db_dicoms.py
@@ -128,4 +128,4 @@ def main(src_dir: str, dst_dir: str):
 
 
 if __name__ == '__main__':
-    main('/home/ubuntu/sdsHD/sd21c015/DataBaseMammography/OMI_SAMPLE', '/home/ubuntu/gmic/omidb_data')
+    main('/home/student/sdsHD/sd21c015/DataBaseMammography/OMI_SAMPLE', '/home/student/gmic/omidb_data')
