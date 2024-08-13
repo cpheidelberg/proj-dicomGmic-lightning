f1ab09f7da3cebc57bd86a070542a737694b11dd
Richard Blažek <blazek.risa@gmail.com>
2024-08-13 02:05:54 +0100

    Only one dataset

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -29,7 +29,7 @@ def run_training(epochs: int, undersampling_rate: float, augmentation_rate: floa
     model_path = './models/'
 
     data_path = '/home/ubuntu/sdsHD/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output'
-    data_dirs = ['/home/ubuntu/gmic/vindrmammo_data', '/home/ubuntu/gmic/omidb_data']
+    data_dirs = ['/home/ubuntu/gmic/vindrmammo_data'] #, '/home/ubuntu/gmic/omidb_data']
     segmentation_path = os.path.join(data_path, 'segmentation')
 
     # set hyperparameters
@@ -99,26 +99,11 @@ def run_training(epochs: int, undersampling_rate: float, augmentation_rate: floa
 
 
 if __name__ == "__main__":
-    print('version_13: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)')
     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
-
-    print('version_14: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=False)')
     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=False)
-
-    print('version_15: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)')
     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
-
-    print('version_16: run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=False)')
     run_training(epochs=16, epoch_smote=16, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=False)
-
-    print('version_17: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=True)')
     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=True)
-
-    print('version_18: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=False)')
     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=False)
-
-    print('version_19: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.5, smote_rate=0.0, binary=True, augment=True)')
     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.5, smote_rate=0.0, binary=True, augment=True)
-
-    print('version_20: run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.5, smote_rate=0.0, binary=True, augment=False)')
     run_training(epochs=16, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.5, smote_rate=0.0, binary=True, augment=False)
