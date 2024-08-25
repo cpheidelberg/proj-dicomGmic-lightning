7611ec22b3c3d4f0d951aaa28e76f00f2b29bb83
Richard Blažek <blazek.risa@gmail.com>
2024-08-25 15:07:17 +0200

    Make it quick, just for testing

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -102,6 +102,6 @@ def run_training(epochs: int, undersampling_rate: float, augmentation_rate: floa
 
 
 if __name__ == "__main__":
-    run_training(epochs=32, epoch_smote=32, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
-    run_training(epochs=32, epoch_smote=32, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
-    run_training(epochs=32, epoch_smote=16, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=True)
+    run_training(epochs=8, epoch_smote=8, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
+    run_training(epochs=8, epoch_smote=8, undersampling_rate=0.5, augmentation_rate=0.0, smote_rate=0.0, binary=True, augment=True)
+    run_training(epochs=8, epoch_smote=4, undersampling_rate=0.0, augmentation_rate=0.0, smote_rate=0.5, binary=True, augment=True)
