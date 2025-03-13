normalisation fixed

src/scripts/optimize.py
@@ -1,81 +1,110 @@
-import os, sys
+import numpy as np
+import argparse, os, cv2, sys
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from tqdm import tqdm
+import time
+import multiprocessing
+
 import optuna
 import torch
-import time
-from lightning.pytorch.callbacks import Timer
-from lightning.pytorch.loggers import TensorBoardLogger, CSVLogger
+from torch.utils.data import random_split
+import lightning.pytorch as pl
+from lightning.pytorch.strategies import DDPStrategy
+from lightning.pytorch.callbacks import ModelSummary, EarlyStopping
+import pydicom as dcm
 
+print('hello world')    
 # import own files 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = "/".join(current_dir.split("/")[:-2])
 sys.path.append(parent_dir)
 
-from src.scripts import training
+from src.utilities import pickling, tools
+from src.modeling import gmic, trainer
+from src.data_loading import loading, dataset
+from src.constants import VIEWS, PERCENT_T_DICT
 
 
-def objective(trial, parameters):
-    # Define hyperparameter search space
-    # parameters["batch_size"] = trial.suggest_categorical("batch_size", [1,2,4,8,16,32])
-    parameters["learning_rate"] = trial.suggest_float("learning_rate", 1e-6, 1e-3, log=True)
-    # augmentation_rate = trial.suggest_float("augmentation_rate", 0, 1)
+def objective(trial, dataTrain, dataValid, dataTest, parameters, model_path):
+
+    crop_shape_val = trial.suggest_int("crop_shape", 128, 500)
+    parameters["crop_shape"] = (crop_shape_val, crop_shape_val)
+
+    lightningModule = trainer.GMICTrainer(
+                        parameters=parameters,
+                        image_class_weights=data_set.class_weights(),
+                        dataset_train=dataTrain,
+                        dataset_valid=dataValid,
+                        dataset_test=dataTest,
+                        model_path = model_path,
+                    )
+    logger = pl.loggers.TensorBoardLogger("tb_logs", name="balanced", log_graph=True)
+
+    early_stop_callback = EarlyStopping(
+                    monitor='val_loss',
+                    patience=5,
+                    strict=False,
+                    verbose=False,
+                    mode='min'
+                )
+    trainer = pl.Trainer(fast_dev_run = False, # default is False. True for running 1 training & 1 validation epoch, int for number of looped batches
+                        # limit_val_batches=0,
+                        # num_sanity_val_steps=0,
+                        max_epochs=parameters["epochs"], 
+                        # gradient_clip_val=1e-3,
+                        accelerator=device, 
+                        # devices=[parameters["gpu_number"]],
+                        devices=[0],
+                        logger=logger,
+                        # profiler="simple",
+                        # strategy=DDPStrategy(find_unused_parameters=True), # ignore unused parameters in network
+                        # callbacks=[ModelSummary(max_depth=2)],
+                    )
+    trainer.fit(model=lightningModule)
     
-    # Create and train the LightningModule
-    trainer = training.run_training(parameters)
+    print("Training finished at: {}".format(time.ctime()))
 
-    # Return the metric to be optimized (e.g., validation loss)
-    best_val_loss = trainer.callback_metrics["val_loss"].item()
+    best_val_loss = trainer.callback_metrics["train_loss"].item()
 
-    print("Run hyperparameters - learning_rate: {}".format(parameters["learning_rate"]))
-    print(f"Loss: {best_val_loss}")
     return best_val_loss
 
 
 if __name__ == "__main__":
 
-    # check if GPU is available
     if torch.cuda.is_available():
-        print(f"{torch.cuda.device_count()} GPUs are available")
         device = "gpu"
-    elif torch.backends.mps.is_available():
-        print("Apple MPS is available")
-        device = "mps"
     else: 
         device = "cpu"
 
-    # set path variables
-    model_path = 'models/'
-    data_dirs = ['../../sdsHD/sd24f004/FFDM/demd/extracted']
-    image_path = '../../sdsHD/sdsHD/sd24f004/FFDM/demd/extracted'
-    output_path = '../../sdsHD/sdsHD/sd24f004/FFDM/demd/predicted'
-    segmentation_path = os.path.join(output_path, 'segmentation')
+    #dicom_file = '1-1.dcm'
+    data_path = '../../../sds_hd/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output/data.pkl'
+    image_path = '/home/ubuntu/gmic/vindrmammo_data'
+    seg_path = '/home/na236/sds_hd/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output/segmentation'
+    output_path = '/home/na236/student1/sds_hd/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output'
+    label_file = "sample_data/annotations/finding_annotations.csv"
+    dict_path = '/home/na236/sds_hd/sd18a006/DataBaseMammography/vindr-mammo/1.0.0/output/dictionary.csv'
+    model_path= '/home/na236/Github_Repos/proj-dicomGmic-lightning/models'
 
-    # set hyperparameters
+    
     parameters = {
-        # training related hyper-parameters
+        # training hyperparameters
         "device_type": device,
         "gpu_number": 0,
-        "epochs": 256,
-        "batch_size": 1,
-        "learning_rate": 3e-5,
+        "epochs": 20,
+        "batch_size": 4,
+        "learning_rate": 0.001,
         "pretrained": True,
         "fine-tuning": False,
         "model_idx": 2,
 
-        "undersampling_rate": 1.0,
-        "augmentation_rate": 0.0,
-        "binary": True,
-        "augment": True,
-        "smote_rate": 0.0,
-        "epoch_smote": 256,
-
         "max_crop_noise": (100, 100),
         "max_crop_size_noise": 100,
-        "data_dirs": data_dirs,
         "image_path": image_path,
-        "segmentation_path": segmentation_path,
+        "segmentation_path": seg_path,
         "output_path": output_path,
-        "model_path": model_path,
 
         # model related hyper-parameters
         "cam_size": (46, 30),
@@ -83,25 +112,26 @@ if __name__ == "__main__":
         "crop_shape": (256, 256), # patch size
         "percent_t": 0.03,
         "post_processing_dim": 256,
-        "num_classes": 2, # output classes (=len(dataset.labels))
+        "num_classes": 6, # output classes
         "use_v1_global": False,
     }
 
-    # Optimize hyperparameters using Optuna
-    study_name = "optTest"
-    storage_name = f"sqlite:///{study_name}.db"
-    study = optuna.create_study(direction="minimize", 
-        pruner=optuna.pruners.MedianPruner(n_startup_trials=3, n_warmup_steps=30),
-        study_name=study_name,
-        storage=storage_name,
-        load_if_exists=True,
-    )
-    study.optimize(lambda trial: objective(trial, parameters), n_trials=16) # n_jobs for multi_processing
-    print("Optimization finished")
+    data_set = dataset.ClassificationImages([image_path], undersampling_rate=0.0, augmentation_rate=0.0, binary=False, augment=False)
 
-    all_trials = study.trials
-    for trial in all_trials:
-        print(f"Trial {trial.number}: Hyperparameters = {trial.params}, Objective Value = {trial.value}")
-    print("Best hyperparameters:", study.best_params)
+    dataTrain, dataValid, dataTest = random_split(data_set, [0.8, 0.1, 0.1])
 
-    print("Training finished at: {}".format(time.ctime()))
+
+    t1 = time.time()
+    study = optuna.create_study(direction="minimize", pruner=optuna.pruners, study_name ='Parameters_1000_6_2', storage='sqlite:///Paratuning_batch.db.sqlite3')
+
+    study.optimize(lambda trial: objective(trial,dataTrain, dataValid, dataTest, parameters, model_path), n_trials=20, timeout=80000)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print(f"Best trial:{trial}")
+    t2 = time.time()
+
+    print(f"Cal. time:{t2 - t1}")