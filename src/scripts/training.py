93d034a5aa50767714869eeb8192a5f7302e46bf
Christoph Blattgerste <christoph.blattgerste@med.uni-heidelberg.de>
2025-03-30 09:59:45 +0000

    minor changes

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -47,7 +47,7 @@ def run_training(parameters):
         reload_dataloaders_every_n_epochs=1,
     )
   
-    trainer.fit(model=model)    
+    trainer.fit(model=model)
     print("Training finished at: ", time.ctime())
     # trainer.test(model=model)
     # trainer.predict(model=model, ckpt_path="GMIC/0veifs4j/checkpoints/epoch=255-step=98816.ckpt")
